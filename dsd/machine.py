ccf4147051c10b0bf3f36ba514bb7217f8578fe9
Piotr Kilczuk <piotr@tymaszweb.pl>
2016-01-03 18:00:20 +0000

    Add support for Class 20 JT

--- dsd/machine.py
+++ dsd/machine.py
@@ -24,8 +24,11 @@ MODEL_MAPPING = {
     'AP_Waggonz.Class90Pack': models.Class90DSDModel,
     'AP_Waggonz.Class90Pack01': models.Class90DSDModel,
     'AP_Waggonz.Class90Pack02': models.Class90DSDModel,
+
+    'JustTrains.Class20': models.GenericDSDModel,
     'JustTrains.NL': models.Class43JTModel,  # TODO: make more specific with patterndict
     'JustTrains.Voyager': models.Class220_221DSDModel,
+
     'RSC.Class70Pack01': models.GenericDSDModel,
     'RSC.GEML': models.Class360DSDModel,
     'RSC.ECMLS': models.GenericDSDModel,
